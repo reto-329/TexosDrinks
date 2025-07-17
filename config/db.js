fb5e23f5eaa40b68fb560420a34662f3291d66d9
reto-329 <reromotabele4love@gmail.com>
2025-07-17 21:33:45 +0100

    Initial commit

--- config/db.js
+++ config/db.js
@@ -1,12 +1,15 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
 });
 
 module.exports = {
